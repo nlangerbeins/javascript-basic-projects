ffcbaca181a6373691cb76c8fe47274c69a5a595
Natallia Langerbeins <natallia.langerbeins@gmail.com>
2023-08-06 23:38:48 +0200

    added 3.reviews

--- 03-reviews/setup/app.js
+++ 03-reviews/setup/app.js
@@ -29,11 +29,12 @@ const reviews = [
     text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
   },
 ];
-// select items
-const img = document.getElementById('person-img');
-const author = document.getElementById('author');
-const job = document.getElementById('job');
-const info = document.getElementById('info');
+
+// sekect items
+const img = document.querySelector('#person-img');
+const author = document.querySelector('#author');
+const job = document.querySelector('#job');
+const info = document.querySelector('#info');
 
 const prevBtn = document.querySelector('.prev-btn');
 const nextBtn = document.querySelector('.next-btn');
@@ -43,42 +44,40 @@ const randomBtn = document.querySelector('.random-btn');
 let currentItem = 0;
 
 // load initial item
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('DOMContentLoaded', () => {
+  showPerson();
+});
+
+// show person based on item
+function showPerson() {
   const item = reviews[currentItem];
   img.src = item.img;
   author.textContent = item.name;
   job.textContent = item.job;
   info.textContent = item.text;
-});
-
-// show person based on item
-function showPerson(person) {
-  const item = reviews[person];
-  img.src = item.img;
-  author.textContent = item.name;
-  job.textContent = item.job;
-  info.textContent = item.text;
 }
+
 // show next person
-nextBtn.addEventListener('click', function () {
+nextBtn.addEventListener('click', () => {
   currentItem++;
   if (currentItem > reviews.length - 1) {
     currentItem = 0;
   }
-  showPerson(currentItem);
+  showPerson();
 });
+
 // show prev person
-prevBtn.addEventListener('click', function () {
+prevBtn.addEventListener('click', () => {
   currentItem--;
   if (currentItem < 0) {
     currentItem = reviews.length - 1;
   }
-  showPerson(currentItem);
+  showPerson();
 });
-// show random person
-randomBtn.addEventListener('click', function () {
-  console.log('hello');
 
-  currentItem = Math.floor(Math.random() * reviews.length);
-  showPerson(currentItem);
+// show random person
+randomBtn.addEventListener('click', () => {
+  const randomPerson = Math.floor(Math.random() * reviews.length);
+  currentItem = randomPerson;
+  showPerson();
 });
