ab68891151bd5d7200c89506d5961337a3afc767
Natallia Langerbeins <natallia.langerbeins@gmail.com>
2023-08-07 13:52:01 +0200

    added 7.questions

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,27 @@
 //using selectors inside the element
 // traversing the dom
+
+// 1.
+// const btns = document.querySelectorAll('.question-btn');
+// btns.forEach((btn) => {
+//   btn.addEventListener('click', (e) => {
+//     const questions = e.currentTarget.parentElement.parentElement;
+//     questions.classList.toggle('show-text');
+//   });
+// });
+
+// 2.
+const questions = document.querySelectorAll('.question');
+
+questions.forEach((question) => {
+  const btn = question.querySelector('.question-btn');
+  btn.addEventListener('click', () => {
+    questions.forEach((item) => {
+      if (item !== question) {
+        item.classList.remove('show-text');
+      }
+    });
+
+    question.classList.toggle('show-text');
+  });
+});
