a53fa57f2ec2682b5be1f975d231b75a880d4b79
Natallia Langerbeins <natallia.langerbeins@gmail.com>
2023-08-11 15:39:28 +0200

    added 10.scroll

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -3,11 +3,85 @@
 // slice extracts a section of a string without modifying original string
 //offsetTop - A Number, representing the top position of the element, in pixels
 
-// ********** set date ************
+// ********** set date in footer ************
+const date = document.querySelector('#date');
+date.innerHTML = new Date().getFullYear();
 
 // ********** close links ************
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
 
+// setup dynamicly!!! links and hamburger button
+navToggle.addEventListener('click', (e) => {
+  // toggle simple setup - not dynamicly
+  //   linksContainer.classList.toggle('show-links');
+
+  // dynamicly
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+});
 // ********** fixed navbar ************
+const navbar = document.querySelector('#nav');
+const topLink = document.querySelector('.top-link');
+
+window.addEventListener('scroll', () => {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+  // scroll
+  if (scrollHeight > navHeight) {
+    navbar.classList.add('fixed-nav');
+  } else {
+    navbar.classList.remove('fixed-nav');
+  }
+
+  // quick button to top
+  if (scrollHeight > 500) {
+    topLink.classList.add('show-link');
+  } else {
+    topLink.classList.remove('show-link');
+  }
+});
 
 // ********** smooth scroll ************
+
 // select links
+const scrollLinks = document.querySelectorAll('.scroll-link');
+scrollLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    // prevent default
+    e.preventDefault();
+
+    // navigate to specific spot
+    const id = e.currentTarget.getAttribute('href').slice();
+    const element = document.querySelector(id);
+
+    // calculate the heights
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains('fixed-nav'); // true or false
+    let position = element.offsetTop - navHeight;
+
+    // should scroll to the right section including navbar
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+
+    // for mobile verison, 82 - height of navbar
+    if (navHeight > 82) {
+      position = position + containerHeight;
+    }
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+
+    linksContainer.style.height = 0;
+  });
+});
