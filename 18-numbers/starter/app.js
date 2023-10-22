d542256f3f0f77d60caa4f6a8d670b81f4a1f53c
Natallia Langerbeins <natallia.langerbeins@gmail.com>
2023-10-22 23:51:17 +0200

    added 18 numbers

--- 18-numbers/starter/app.js
+++ 18-numbers/starter/app.js
@@ -1,1 +1,23 @@
-console.log('numbers project');
+const items = [...document.querySelectorAll('.number')];
+
+const updateCount = (el) => {
+  const value = parseInt(el.dataset.value);
+  const increment = Math.ceil(value / 1000);
+  let initial = 0;
+
+  const increaseCount = setInterval(() => {
+    initial += increment;
+
+    if (initial > value) {
+      el.textContent = `${value}+`;
+      clearInterval(increaseCount);
+      return;
+    }
+
+    el.textContent = initial;
+  }, 1);
+};
+
+items.forEach((item) => {
+  updateCount(item);
+});
