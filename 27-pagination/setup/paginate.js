92ded8e582c6379cc255398e535993c57f9191ad
Natallia Langerbeins <natallia.langerbeins@gmail.com>
2023-11-13 23:07:13 +0100

    added 27 pagination

--- 27-pagination/setup/paginate.js
+++ 27-pagination/setup/paginate.js
@@ -1,3 +1,12 @@
-const paginate = () => {}
+const paginate = (followers) => {
+  const itemsPerPage = 10;
+  const numberOfPages = Math.ceil(followers.length / itemsPerPage);
+  console.log(numberOfPages);
+  const newFollowers = Array.from({ length: numberOfPages }, (_, index) => {
+    const start = index * itemsPerPage;
+    return followers.slice(start, start + itemsPerPage);
+  });
+  return newFollowers;
+};
 
-export default paginate
+export default paginate;
